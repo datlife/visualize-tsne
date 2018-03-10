9ab44fc0b906c17dca18ff65502c53e64c7a7ccf
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-03-09 17:21:42 -0800

    Updated KerasLogger

--- kerasHook.py
+++ kerasHook.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+
 from tensorflow.python.training import training_util
 from tensorflow.python.training.session_run_hook import SessionRunArgs
 from tensorflow.python.training.basic_session_run_hooks import _as_graph_element
@@ -7,18 +9,20 @@ class KerasLogger(tf.train.SessionRunHook):
   """Write metrics to std out
 
   """
-  def __init__(self, tensors, epochs, steps_per_epoch, curr_epoch=0):
+  def __init__(self, epochs, steps_per_epoch, tensors_to_log=None):
     self._epochs = epochs
     self._step_per_epoch = steps_per_epoch
-    self._curr_epoch = curr_epoch
-    self._curr_step = 0
 
-    if not isinstance(tensors, dict):
-      self._tag_order = tensors
-      tensors = {item: item for item in tensors}
+    if tensors_to_log is not None:
+      if not isinstance(tensors_to_log, dict):
+        self._tag_order = tensors_to_log
+        tensors_to_log = {item: item for item in tensors_to_log}
+      else:
+        self._tag_order = tensors_to_log.keys()
+      self._tensors = tensors_to_log
+      self._log_tensors = True
     else:
-      self._tag_order = tensors.keys()
-    self._tensors = tensors
+      self._log_tensors = False
 
   def begin(self):
     self._global_step_tensor = training_util._get_global_step_read()
@@ -27,26 +31,48 @@ class KerasLogger(tf.train.SessionRunHook):
         "Global step should be created to use KerasLogger")
 
     # Convert names to tensors if given
-    self._current_tensors = {tag: _as_graph_element(tensor)
-                             for (tag, tensor) in self._tensors.items()}
+    if self._log_tensors:
+      self._current_tensors = {tag: _as_graph_element(tensor)
+                               for (tag, tensor) in self._tensors.items()}
+
+  def after_create_session(self, session, coord):
+    # Init current_epoch and current_step
+    self._curr_step = session.run(self._global_step_tensor)
+    self._curr_epoch = int(np.floor(self._curr_step / self._step_per_epoch))
+    self._curr_step -= self._curr_epoch * self._step_per_epoch
+    self._first_run = True
+
+    if self._curr_step is not 0:
+      print("Resuming training from global step(s): %s...\n" % self._curr_step)
 
   def before_run(self, run_context):
-    if self._curr_step % self._step_per_epoch == 0:
+    if self._first_run is  True:
+      print('Epoch %s/%s:' % (self._curr_epoch, self._epochs))
+      self.progbar = tf.keras.utils.Progbar(target=self._step_per_epoch)
+      self._first_run = False
+
+    elif self._curr_step % self._step_per_epoch == 0:
       self._curr_epoch += 1
       self._curr_step = 0
       print('Epoch %s/%s:' % (self._curr_epoch, self._epochs))
       self.progbar = tf.keras.utils.Progbar(target=self._step_per_epoch)
 
-    return SessionRunArgs(self._current_tensors)
+    if self._log_tensors:
+      return SessionRunArgs(self._current_tensors)
+
 
   def after_run(self,
                 run_context,  # pylint: disable=unused-argument
                 run_values):
-    values = self._log_tensors(run_values.results)
+    if self._log_tensors:
+      values = self._extract_tensors_info(run_values.results)
+    else:
+      values = None
+
     self._curr_step += 1
     self.progbar.update(self._curr_step, values=values)
 
-  def _log_tensors(self, tensor_values):
+  def _extract_tensors_info(self, tensor_values):
     stats = []
     for tag in self._tag_order:
       stats.append((tag, tensor_values[tag]))
